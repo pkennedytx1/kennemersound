c9f95e4f89297bdfc3500a1553144f4468b0d265
Patrick Glen Kennedy <pkennedytx1@Patricks-MacBook-Pro.local>
2019-06-29 15:42:42 -0500

    added real

--- my-app/src/components/Reel/index.js
+++ my-app/src/components/Reel/index.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import ReactPlayer from 'react-player';
+import './style.css'
 
 class Reel extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state ={
+            bgColor: 'white'
+        }
+    }
+
     render() {
         return(
-            <div style={{height: '900px', padding: '30px'}} className='bg-light'>
+            <div className='reelcontainer' style={{height: '900px', padding: '30px', backgroundColor: this.state.bgColor}}>
                 <h1 id='Reel' className='text-center'>Reel</h1>
+                <div className='player-wrapper'>
+                    <ReactPlayer
+                        className='react-player'
+                        url={'https://vimeo.com/279542470'}
+                        config={{
+                            vimeo: {
+                                preload: true
+                            }
+                        }}
+                        width='100%'
+                        height='100%'
+                        onStart={() => {
+                            this.setState({
+                                bgColor: 'black'
+                            })
+                        }}
+                        onEnded={() => {
+                            this.setState({
+                                bgColor: 'white'
+                            })
+                        }}
+                    />
+                </div>
             </div>
         )
     }
