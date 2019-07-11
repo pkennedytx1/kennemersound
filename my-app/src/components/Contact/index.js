30f4bdfd609f97cb13626ab00d40b4b2134c106d
Patrick Glen Kennedy <pkennedytx1@Patricks-MacBook-Pro.local>
2019-07-10 23:35:32 -0400

    added form validation for name, need to add validation for email and error messages

--- my-app/src/components/Contact/index.js
+++ my-app/src/components/Contact/index.js
@@ -4,13 +4,35 @@ class Contact extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            name: 'form-group',
-            email: 'form-group',
-            message: 'form-group',
+            name: 'form-control',
+            email: 'form-control',
+            message: 'form-control',
             nameInput: '',
             emailInput: '',
             messageInput: ''
         }
+    }
+
+    inputNameChange = e => {
+        this.setState({
+            nameInput: e.target.value
+        })
+        if(this.state.nameInput.length > 5) {
+            this.setState({
+                name: 'form-control is-valid'
+            })
+            console.log('yes')
+        } else {
+            this.setState({
+                name: 'form-control'
+            })
+        }
+    }
+
+    inputEmailChange = e => {
+        this.setState({
+            emailInput: e.target.value
+        })
     }
 
     render() {
@@ -35,12 +57,29 @@ class Contact extends React.Component {
                         </div>
                         <div style={{backgroundColor: 'black', width:'50%', zIndex: '1', position: 'relative', height: '400px', float: 'left'}} className='contactleft'>
                             <h3 style={{color: 'white', fontWeight: 'bold', padding: '20px 30px 0 30px'}}>Send Me a Message.</h3>
-                            <div style={{width: '100%', float: 'right', padding: '30px'}} class="form-group">
-                                <input style={{borderRadius: '0', backgroundColor: 'black'}} type="name" class="form-control" id="exampleInputName1" aria-describedby="nameHelp" placeholder="Enter Name" />
+                            <div style={{width: '100%', float: 'right', padding: '30px'}} class='form-group'>
+                                <input style={{borderRadius: '0', backgroundColor: 'black'}} 
+                                type="name" 
+                                class={this.state.name} 
+                                id="exampleInputName1" 
+                                aria-describedby="nameHelp" 
+                                placeholder="Enter Name"
+                                value={this.state.nameInput}
+                                onChange={this.inputNameChange} />
                             </div>
-                            <div style={{width: '100%', float: 'right', padding: '0 30px 30px 30px'}} class="form-group">
-                                <input style={{borderRadius: '0', backgroundColor: 'black'}} type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Email" />
+                            <div style={{width: '100%', float: 'right', padding: '0 30px 30px 30px'}} class="form-group has-successs">
+                                <input style={{borderRadius: '0', backgroundColor: 'black'}} 
+                                type="email" 
+                                class="form-control" 
+                                id="exampleInputEmail1" 
+                                aria-describedby="emailHelp" 
+                                placeholder="Enter Email"
+                                value={this.state.emailInput}
+                                onChange={this.inputEmailChange} />
                             </div>
+                            <button type="button" 
+                            class="btn btn-outline-secondary"
+                            onClick={this.onSumbit}>Send Message</button>
                         </div>
                     </div>
                 </div>
