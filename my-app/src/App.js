27ec2be0fe0165a630d2ad8f5249d66c04ed8162
Patrick Glen Kennedy <pkennedytx1@Patricks-MacBook-Pro.local>
2019-07-29 16:55:20 -0500

    added the setup for the scrollspy classes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,23 +7,42 @@ import Projects from './components/Projects'
 import Aboutme from './components/Aboutme'
 import Testimonials from './components/Testimonials'
 import Contact from './components/Contact'
-import ProgressBar from "react-scroll-progress-bar"
+import { Waypoint } from 'react-waypoint'
 
 import './App.css';
 
-function App() {
-  return (
-    <div>
-        <Logo />
-        <Navbar />
-          <Landing />
-          <Reel id='Reel'/>
-          <Projects id='Projects'/>
-          <Aboutme id='Aboutme'/>
-          <Testimonials id='Testimonials'/>
-          <Contact id='Contact'/>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state={
+        reel: 'nav-link',
+        projects: 'nav-link',
+        aboutme: 'nav-link',
+        testimonials: 'nav-link',
+        contactme: 'nav-link'
+    }
+  }
+
+  render() {
+    return (
+      <div>
+          <Logo />
+          <Navbar 
+            reel={this.state.reel}/>
+            <Landing />
+            <Waypoint
+              onEnter={console.log('entered reel')}
+            >
+            <Reel id='Reel' />
+            </Waypoint>
+            <Projects id='Projects'/>
+            <Aboutme id='Aboutme'/>
+            <Testimonials id='Testimonials'/>
+            <Contact id='Contact'/>
+      </div>
+    );
+  }
 }
 
 export default App;
